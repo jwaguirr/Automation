3e0875164903da170e5d07e605bb1a57ae6d7eff
jwaguirr <jwaguirre.23@gmail.com>
2023-01-12 23:09:56 -0600

    Big Update LOL

--- Room-Automation/web.py
+++ Room-Automation/web.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, redirect, request, url_for
+from datetime import datetime
+import time
 import sqlite3 as sql
+import threading
+
+# To run the app flask --app web --debug run --port 5001
 
 app = Flask(__name__)
 conn = sql.connect("database.db", check_same_thread=False)
@@ -9,16 +14,26 @@ c = conn.cursor()
 c.execute("""CREATE TABLE IF NOT EXISTS times(
     prioritized text,
     weekdays text,
-    weekends text
+    weekends text,
+    weekdayNights text,
+    weekendNights text,
+    cancel int
 )
 """)
+
+def formatTime(amPm, tableValName, userTimeInput):
+    alarmTime = "{} {}".format(userTimeInput, amPm)
+    inTime = datetime.strptime(alarmTime, "%I:%M %p")
+    outTime = datetime.strftime(inTime, "%H:%M")
+    outTimeSplit = outTime.split()
+    checkTimeFormat(outTime,tableValName, outTimeSplit)
 
 # Checking for an empty DB-- allows for updating data
 def checkValues():
     c.execute("SELECT * FROM times")
     times = c.fetchone()
     if times == None:
-        c.execute("INSERT INTO times VALUES (' ', ' ', ' ')")
+        c.execute("INSERT INTO times VALUES (' ', ' ', ' ', ' ', ' ', ' ')")
         print("Created Query")
 
 # This simply prints the values of the DB
@@ -27,27 +42,27 @@ def printDB(dbMessage):
     c.execute("SELECT * FROM times")
     times = c.fetchall()
     for items in times:
-        setAlarm(items)
         print(items)
 
 # Checks the format of the input, ie. xx:xx if correct, adds to the DB
-def checkTimeFormat(var, val, com):
-    for x in var:
+def checkTimeFormat(outTime, tableValName, outTimeSplit):
+    print(outTimeSplit)
+    for x in outTimeSplit:
         try:
-            if( (type(int(x[0])) == int) and (type(int(x[1])) == int) and (x[2] == ":") and (type(int(x[3]) == int) and (type(int(x[4]) == int)) and (len(x) == 5))):
-                print("Made it")
-                updateDB = 'UPDATE times SET {} = (?)'.format(val)
-                c.execute(updateDB, (com,))
+            if( (type(int(x[0])) == int) and (type(int(x[1])) == int) and (x[2] == ":") and (type(int(x[3]) == int) and (type(int(x[4]) == int)) )):
+                updateDB = 'UPDATE times SET {} = (?)'.format(tableValName)
+                c.execute(updateDB, (outTime,))
 
             else:
                 # TODO add error handling to the front end
                 print("Something went wrong")
         except ValueError as e:
             print("Something went wrong, please make sure its in 24 hour format", e)
-#    setAlarm()
 
 # Compares value to database, if different runs moves to format checking
-def pullDifferences(prioritized, weekdays, weekends ):
+def pullDifferences(prioritized, weekdays, weekends, weekdayNights, weekendNights, priorityAmPm):
+    if priorityAmPm == None:
+        pass
     c.execute("SELECT * FROM times")
     times = c.fetchall()
     for items in times:
@@ -56,58 +71,147 @@ def pullDifferences(prioritized, weekdays, weekends ):
                 c.execute('UPDATE times SET prioritized = "None"')
                 conn.commit()
             else:
-                val = "prioritized"
-                var = prioritized.split()
-                com = prioritized
-                checkTimeFormat(var,val,com)
+                tableValName = "prioritized"
+                amPm = priorityAmPm
+                userTimeInput = prioritized
+                formatTime(amPm,tableValName,userTimeInput)
 
         if items[1] != weekdays:
             if weekdays == "":
                 pass
             else:
-                val = "weekdays"
-                var = weekdays.split()
-                com = weekdays
-                checkTimeFormat(var,val,com)
+                tableValName = "weekdays"
+                amPm = "AM"
+                userTimeInput = weekdays
+                formatTime(amPm, tableValName,userTimeInput)
 
         if items[2] != weekends:
             if weekends == "":
                 pass
             else:
-                val = "weekends"
-                var = weekends.split()
-                com = weekends
-                checkTimeFormat(var,val,com)
+                tableValName = "weekends"
+                amPm = "AM"
+                userTimeInput = weekends
+                formatTime(amPm, tableValName,userTimeInput)
+
+        if items[3] != weekdayNights:
+            if weekdayNights == "":
+                pass
+            else:
+                tableValName = "weekdayNights"
+                amPm = "PM"
+                userTimeInput = weekdayNights
+                formatTime(amPm, tableValName,userTimeInput)    
+
+        if items[4] != weekendNights:
+            if weekendNights == "":
+                pass
+            else:
+                tableValName = "weekendNights"
+                amPm = "PM"
+                userTimeInput = weekendNights
+                formatTime(amPm, tableValName,userTimeInput)    
+
+
+
 
 # Setting the alarm when pulling from the DB
-def setAlarm(items):
-    prioritizedFromDB = items[0]; weekdaysFromDB = items [1]; weekendsFromDB = items[2]
-    if(prioritizedFromDB != "None"):
-        #TODO add actual alarm feature
+def setAlarm():
+    c.execute("SELECT * FROM times")
+    times = c.fetchall()
+    for items in times:
         pass
+    if items[5] != 1:
+        alarms = 0
+    else:
+        alarms = 1
+    while alarms == 1:
+        #TODO add priority functionality
+        c.execute("SELECT * FROM times")
+        times = c.fetchall()
+        for items in times:
+            pass
+        prioritizedFromDB = "{}{}".format(items[0],":00"); weekdaysFromDB = "{}{}".format(items [1],":00"); weekendsFromDB = "{}{}".format(items[2],":00"); weekdayNightsFromDB = "{}{}".format(items[3],":00"); weekendNightsFromDB = "{}{}".format(items[4],":00"); toggleAlarmFromDB = items[5]
+        if toggleAlarmFromDB == 0:
+            alarms = 0
+        dayOfWeek = int((datetime.now().date()).strftime('%w'))
+        itsAWeekend = dayOfWeek == 0 or dayOfWeek == 6
+        now = datetime.now()
+        currentTime = now.strftime("%H:%M:%S")
+        if itsAWeekend:
+            if currentTime == weekendsFromDB:
+                print("we did it weekend day time")
+            elif currentTime == weekendNightsFromDB:
+                print("We did it weekend night time")
+        else: # Not a weekend
+            if currentTime == weekdaysFromDB:
+                print("We did it weekday daytime")
+            elif currentTime == weekdayNightsFromDB:
+                print("We did it weekday nighttime!")
+        print(threading.active_count())
+        print(currentTime)
+        time.sleep(1)
 
+        
 @app.route('/', methods = ["GET", "POST"])
 def index():
+    checkValues()
     if request.method == "POST":
         try:
-            checkValues()
-            prioritized = request.form['prioritized']
-            weekdays = request.form['weekdays']
-            weekends = request.form['weekends']
-            pullDifferences(prioritized,weekdays,weekends)
-            conn.commit()
-        except TypeError as e:
-            print("Exception Thrown!", e)
+            # Checks if alarms are canceled
+            alarmToggle = request.form['toggleAlarms']
+            if alarmToggle == "Toggle":
+                c.execute("SELECT * FROM times")
+                alarms = c.fetchall()
+                for x in alarms: 
+                    if x[5] == 1:
+                        toggle = 0
+                    else:
+                        toggle = 1
+                c.execute("UPDATE times SET cancel = (?)", (toggle,))
+                conn.commit()
+        except:
+            try:
+                prioritized = request.form['prioritized']
+                try:
+                    priorityAmPm = request.form['prioritizedTime']
+                except:
+                    priorityAmPm = None
+                weekdays = request.form['weekdays']
+                weekends = request.form['weekends']
+                weekendNights = request.form['weekendNights']
+                weekdayNights = request.form['weekdayNights']
+                pullDifferences(prioritized,weekdays,weekends, weekdayNights, weekendNights, priorityAmPm)
+                conn.commit()
+            except TypeError as e:
+                print("Exception Thrown!", e)
     printDB(dbMessage="Searching db")
     c.execute("SELECT * FROM times")
     data = c.fetchall()
-    return render_template('index.html', data = data)
+    for items in data:
+        if items[5] == 1:
+            alarm = "Turn Off Alarms"
+        else:
+            alarm = "Turn On Alarms"
+    t1 = threading.Thread(target=setAlarm)
+    if threading.active_count() > 2:
+        pass
+    else:
+        t1.start()
+    return render_template('index.html', data = data, alarm = alarm)
 
 @app.route('/database', methods = ['POST', 'GET'])
 def postDB():
     c.execute("SELECT * FROM times")
     data = c.fetchall()
-    return render_template('template.html', data = data)
+    for x in data:
+        if x[5] == 1:
+            alarm = "On"
+        else:
+            alarm = "Off"
+    return render_template('template.html', data = data, alarm = alarm)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, use_reloader=False, port=5000)
+
+# TO RUN ON HOST MACHINE host="0.0.0.0"
--- test.py
+++ test.py
@@ -1,2 +1,14 @@
-val = "test"
-val.split()
+from datetime import datetime
+import time
+import threading
+
+def activeTime():
+    while True:
+        print(threading.active_count())
+        now = datetime.now()
+        currentTime = now.strftime("%H:%M:%S")
+        print(currentTime)
+        time.sleep(1)
+
+t1 = threading.Thread(target = activeTime)
+t1.start()
